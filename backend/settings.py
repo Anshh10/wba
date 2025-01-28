e64ec739f812e4881717d51bfb8ed7f9a561ba16
Anshh10 <anshhwork@gmail.com>
2025-01-28 20:54:36 +0530

    changes 2

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-z3(-wfe3-b1fk8h7^1nq01@iw4faf&lyz64t7-05on)z%lv1y*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
